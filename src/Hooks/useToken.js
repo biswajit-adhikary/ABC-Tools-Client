779dcc97154461c22d488a4c723ab2cbe9865e24
biswajit-adhikary <biswajitadhikary.web@gmail.com>
2022-05-25 00:38:24 +0600

    Fixed JWT issue

--- src/Hooks/useToken.js
+++ src/Hooks/useToken.js
@@ -1,18 +1,25 @@
-import axios from "axios";
 import { useEffect, useState } from "react"
 
 const useToken = user => {
     const [token, setToken] = useState('');
     useEffect(() => {
-        const getToken = async () => {
-            const email = user?.user?.email;
-            if (email) {
-                const { data } = await axios.post('http://localhost:5000/login', { email });
-                setToken(data.accessToken);
-                localStorage.setItem('accessToken', data.accessToken);
-            }
+        const email = user?.user?.email;
+        const newUser = { email: email };
+        if (email) {
+            fetch(`http://localhost:5000/user/${email}`, {
+                method: 'PUT',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(newUser)
+            })
+                .then(res => res.json())
+                .then(data => {
+                    const accessToken = data.token;
+                    localStorage.setItem('accessToken', accessToken);
+                    setToken(accessToken);
+                })
         }
-        getToken();
     }, [user]);
     return [token];
 }
