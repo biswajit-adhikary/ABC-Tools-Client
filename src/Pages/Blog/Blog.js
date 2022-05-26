20f8f3f05715a64d69fcfa26f72d30f829324f81
biswajit-adhikary <biswajitadhikary.web@gmail.com>
2022-05-26 23:02:39 +0600

    Blog added

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -26,7 +26,7 @@ const Blog = () => {
                             <img src={blogOne} alt="" />
                             <div className="blog-text p-3">
                                 <h2>How will you improve the performance of a React Application?</h2>
-                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore libero nostrum expedita totam maiores, veritatis non omnis porro cumque sit vel numquam nobis. Ut laboriosam qui eligendi odit! Veniam consequatur ipsa porro iusto dolorum vero fugit ex nam aperiam, similique repellat esse perferendis delectus, dolores et culpa totam, animi obcaecati nostrum quibusdam voluptate qui beatae eum dolor? Commodi iusto veniam officiis fugiat aperiam error minus!</p>
+                                <p>There are many ways to improve react app performance. Using immutable data structure is the best way to improve react application performance.  Dependency optimization is a very good way to improve react app performance. Use fragments for HTML element rappers is a very good way. We should avoid index number for key map. Also need to avoid props in initial state. </p>
                             </div>
                         </div>
                     </Col>
@@ -35,7 +35,7 @@ const Blog = () => {
                             <img src={blogTwo} alt="" />
                             <div className="blog-text p-3">
                                 <h2>What are the different ways to manage a state in a React application?</h2>
-                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore libero nostrum expedita totam maiores, veritatis non omnis porro cumque sit vel numquam nobis. Ut laboriosam qui eligendi odit! Veniam consequatur ipsa porro iusto dolorum vero fugit ex nam aperiam, similique repellat esse perferendis delectus, dolores et culpa totam, animi obcaecati nostrum quibusdam voluptate qui beatae eum dolor? Commodi iusto veniam officiis fugiat aperiam error minus!</p>
+                                <p>There are 4 way to manage react app state. They are Local state, Global state, Server state, URL state. Local state is a UI State. It manage use useState hook. Global state also UI state. Using global state we can manage multiple components easily. For external server date we use Server State. Data that exists on our URLs called URL state.</p>
                             </div>
                         </div>
                     </Col>
@@ -44,7 +44,7 @@ const Blog = () => {
                             <img src={blogThree} alt="" />
                             <div className="blog-text p-3">
                                 <h2>How does prototypical inheritance work?</h2>
-                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore libero nostrum expedita totam maiores, veritatis non omnis porro cumque sit vel numquam nobis. Ut laboriosam qui eligendi odit! Veniam consequatur ipsa porro iusto dolorum vero fugit ex nam aperiam, similique repellat esse perferendis delectus, dolores et culpa totam, animi obcaecati nostrum quibusdam voluptate qui beatae eum dolor? Commodi iusto veniam officiis fugiat aperiam error minus!</p>
+                                <p>Prototypal Inheritance is ES6 a feature in javascript. Prototypical inheritance use for add methods and properties in objects. It makes easy to sharing a function, data value etc. JavaScript is a prototype-capable languages and prototypical inheritance make this language more powerful. It will save your hours of coding.</p>
                             </div>
                         </div>
                     </Col>
@@ -53,11 +53,11 @@ const Blog = () => {
                             <img src={blogFour} alt="" />
                             <div className="blog-text p-3">
                                 <h2>Why you do not set the state directly in React. For example, if you have const [products, setProducts] = useState([]). Why you do not set products = [...] instead, you use the setProducts</h2>
-                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore libero nostrum expedita totam maiores, veritatis non omnis porro cumque sit vel numquam nobis. Ut laboriosam qui eligendi odit! Veniam consequatur ipsa porro iusto dolorum vero fugit ex nam aperiam, similique repellat esse perferendis delectus, dolores et culpa totam, animi obcaecati nostrum quibusdam voluptate qui beatae eum dolor? Commodi iusto veniam officiis fugiat aperiam error minus!</p>
+                                <p>In React we should not update the state directly. The main reason we don't use products = [...] instead because it will lost controls of the state for all components. If we set it directly , it will replace the updates you made every state. It will not change the state, but update it directly. It will return the present value only.</p>
                             </div>
                         </div>
                     </Col>
-                    <Col lg={4}>
+                    {/* <Col lg={4}>
                         <div className="single-blog">
                             <img src={blogFive} alt="" />
                             <div className="blog-text p-3">
@@ -65,13 +65,13 @@ const Blog = () => {
                                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore libero nostrum expedita totam maiores, veritatis non omnis porro cumque sit vel numquam nobis. Ut laboriosam qui eligendi odit! Veniam consequatur ipsa porro iusto dolorum vero fugit ex nam aperiam, similique repellat esse perferendis delectus, dolores et culpa totam, animi obcaecati nostrum quibusdam voluptate qui beatae eum dolor? Commodi iusto veniam officiis fugiat aperiam error minus!</p>
                             </div>
                         </div>
-                    </Col>
+                    </Col> */}
                     <Col lg={4}>
                         <div className="single-blog">
                             <img src={blogSix} alt="" />
                             <div className="blog-text p-3">
                                 <h2>What is a unit test? Why should write unit tests?</h2>
-                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore libero nostrum expedita totam maiores, veritatis non omnis porro cumque sit vel numquam nobis. Ut laboriosam qui eligendi odit! Veniam consequatur ipsa porro iusto dolorum vero fugit ex nam aperiam, similique repellat esse perferendis delectus, dolores et culpa totam, animi obcaecati nostrum quibusdam voluptate qui beatae eum dolor? Commodi iusto veniam officiis fugiat aperiam error minus!</p>
+                                <p>Unit testing is a application development process. It's very important for getting best output from your application. This testing process is a very complex process and need to work different ways. If we don't do the unit testing correctly, this is very bad for the application. Unit testing can performed manually or automated.</p>
                             </div>
                         </div>
                     </Col>
